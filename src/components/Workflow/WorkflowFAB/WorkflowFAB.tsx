25bd6588bf86d292755ff19eec419ad11077467f
kimminhyug <kimminhyug@naver.com>
2025-09-29 12:31:35 +0900

    워크플로우 에디터 컨트롤 버튼 영역을 FAB UI으로 전환 (#4)
    - 기존 control menu ui를 Floating Action Button ui 형태로 변경
    - 토글(실행, 정지), 토글(일시 정지, 계쏙 진행), 단일(다운로드) 형태로 변경
    - fluent ui icon viewbox 이슈로 react-icons 라이브러리를 추가

---
+++ src/components/Workflow/WorkflowFAB/WorkflowFAB.tsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import { useWorkflow } from '../../../hooks/useWorkflow';
+
+import {
+  FaFileDownload,
+  FaMinus,
+  FaPauseCircle,
+  FaPlayCircle,
+  FaPlus,
+  FaRegStopCircle,
+} from 'react-icons/fa';
+import { RxResume } from 'react-icons/rx';
+export const WorkflowFAB = () => {
+  const {
+    simulateExecution,
+    pauseExecution,
+    executionState,
+    resumeExecution,
+    stopExecution,
+    exportWorkflowJSON,
+  } = useWorkflow();
+
+  const [open, setOpen] = useState(false);
+
+  const isRunning = executionState === 'running';
+  const isPaused = executionState === 'paused';
+
+  // 실행 중이면 정지, 일시정지 중이면 정지, 아니면 시뮬레이션 시작
+  const handleMainExecution = () => {
+    if (isRunning || isPaused) {
+      stopExecution();
+    } else {
+      simulateExecution();
+    }
+  };
+
+  // 실행 중이면 일시정지, 일시정지 중이면 재개
+  const handlePauseResume = () => {
+    if (isRunning) pauseExecution();
+    else if (isPaused) resumeExecution();
+  };
+  // 실행 중이면 일시 중지면 정지, 그 외 시작
+  const renderExecutionIcon = () => {
+    if (isRunning || isPaused) return <FaRegStopCircle />;
+    return <FaPlayCircle />;
+  };
+  // 실행 중이면 일시정지 , 일시정지 면 재게, 그 외 일시 정지
+  const renderPauseResumeIcon = () => {
+    if (isRunning) return <FaPauseCircle />;
+    if (isPaused) return <RxResume />;
+    return <FaPauseCircle />;
+  };
+
+  return (
+    <div className="fab-container">
+      <div className="fab-main fab-sub" onClick={() => setOpen(!open)}>
+        {open ? <FaMinus /> : <FaPlus />}
+      </div>
+
+      {/* 서브 버튼들 */}
+      <div className={`fab-menu ${open ? 'open' : ''}`}>
+        <div className="fab-sub" onClick={handleMainExecution}>
+          {renderExecutionIcon()}
+        </div>
+        <div className="fab-sub" onClick={handlePauseResume}>
+          {renderPauseResumeIcon()}
+        </div>
+        <div className="fab-sub" onClick={exportWorkflowJSON}>
+          <FaFileDownload />
+        </div>
+      </div>
+    </div>
+  );
+};
